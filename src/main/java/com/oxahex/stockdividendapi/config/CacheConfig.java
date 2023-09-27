53f83545db71ff11167a6672bd2a36d1bd808c38
oxahex <oxahex@gmail.com>
2023-09-27 12:59:31 +0900

    fix: 괄호 닫음

--- src/main/java/com/oxahex/stockdividendapi/config/CacheConfig.java
+++ src/main/java/com/oxahex/stockdividendapi/config/CacheConfig.java
@@ -31,7 +31,7 @@ public class CacheConfig {
         RedisCacheConfiguration conf =
                 RedisCacheConfiguration.defaultCacheConfig()
                         .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
                         return RedisCacheManager.RedisCacheManagerBuilder
                                 .fromConnectionFactory(redisConnectionFactory)
