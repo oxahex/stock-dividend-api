9b856edcec1f9bbcb9ae63a626a937560883f48f
oxahex <oxahex@gmail.com>
2023-09-27 12:22:50 +0900

    feat: 스크래핑 스케줄링 기능 구현
    - 스레드풀 처리

---
+++ src/main/java/com/oxahex/stockdividendapi/scheduler/ScraperScheduler.java
@@ -0,0 +1,65 @@
+package com.oxahex.stockdividendapi.scheduler;
+
+import com.oxahex.stockdividendapi.model.Company;
+import com.oxahex.stockdividendapi.model.ScrapedResult;
+import com.oxahex.stockdividendapi.persist.CompanyRepository;
+import com.oxahex.stockdividendapi.persist.DividendRepository;
+import com.oxahex.stockdividendapi.persist.entity.CompanyEntity;
+import com.oxahex.stockdividendapi.persist.entity.DividendEntity;
+import com.oxahex.stockdividendapi.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ScraperScheduler {
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+    private final Scraper yahooFinanceScraper;
+
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    public void yahooFinanceScheduling() {
+        // 저장된 회사 목록을 조회
+        List<CompanyEntity> companyEntityList =
+                this.companyRepository.findAll();
+
+        // 회사마다 배당금 정보를 새로 스크래핑
+        for (CompanyEntity companyEntity : companyEntityList) {
+            log.info("Scraping Scheduler is Started -> " + companyEntity.getName());
+
+            ScrapedResult scrapedResult =
+                    yahooFinanceScraper.scrap(
+                            Company.builder()
+                                    .name(companyEntity.getName())
+                                    .ticker(companyEntity.getTicker())
+                                    .build()
+                    );
+
+            // 스크래핑한 배당금 정보 중 DB에 없는 값은 저장
+                // dividend 모델 -> 엔티티로 매핑
+                // 하나씩 DB에 저장
+            scrapedResult.getDividendList().stream()
+                    .map(e -> new DividendEntity(companyEntity.getId(), e))
+                    .forEach(e -> {
+                        boolean exists = this.dividendRepository
+                                .existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) this.dividendRepository.save(e);
+                    });
+
+            // 연속적으로 스크래핑 대상 사이트 서버에 요청을 넣지 않도록 일시 정지
+            try {
+                Thread.sleep(3000);     // 3초간 정지
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();     // 현재 스레드에 interrupt
+            }
+        }
+
+    }
+}
